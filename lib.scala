00f61eb5f6f38bc22e759c4a9c663ebd9341f277
Paweł Marks <pmarks@virtuslab.com>
2023-04-13 12:33:22 +0200

    Add publishing info

--- lib.scala
+++ lib.scala
@@ -1,3 +1,14 @@
 //> using scala "3.2.2"
 //> using jvm "8"
 //> using file "src"
+
+//> using publish.organization "pro.kordyjan"
+//> using publish.name "typelogger"
+//> using publish.computeVersion "git:tag"
+
+//> using publish.repository "central-s01"
+
+//> using publish.license "Apache-2.0"
+//> using publish.url "https://github.com/Kordyjan/typeLogger"
+//> using publish.vcs "github:Kordyjan/typeLogger"
+//> using publish.developer "Kordyjan|Paweł Marks|https://github.com/Kordyjan"
